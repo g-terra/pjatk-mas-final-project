6379dc932409a3b0d7d71fd13ba21699e33ada47
gmiranda <gmiranda@akamai.com>
2022-06-13 11:04:27 +0200

    initial API commit

---
+++ device-manufacture-api/src/main/java/pjatk/mas/finalproject/devicemanufactureapi/DeviceManufactureApiApplication.java
@@ -0,0 +1,13 @@
+package pjatk.mas.finalproject.devicemanufactureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManufactureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceManufactureApiApplication.class, args);
+	}
+
+}
---
+++ device-manufacture-api/src/test/java/pjatk/mas/finalproject/devicemanufactureapi/DeviceManufactureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pjatk.mas.finalproject.devicemanufactureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManufactureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
