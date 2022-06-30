5e5b6178a9e24deb97db4c39611c771924cd703c
gmiranda <gmiranda@akamai.com>
2022-06-30 20:32:41 +0200

    refact i unused code for 404

--- device-manufacture-ui/pages/404.js
+++ device-manufacture-ui/pages/404.js
@@ -1,32 +1,13 @@
-import { useEffect, useState } from 'react';
-import { Box, Collapse, LinearProgress, Stack, TextField, Typography } from '@mui/material';
-import axios from 'axios';
+import { Box, Typography } from '@mui/material';
 import Head from 'next/head';
 import alertImage from '../public/alert.png';
 import Image from 'next/image';
 
 export default function Search() {
-
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [searched, setSearched] = useState("");
-    const [displayData, setDisplayData] = useState(data);
-
-    useEffect(() => {
-        axios.get(process.env.deviceManufactureApi + "/device-type").then(response => {
-            setData(response.data);
-            setDisplayData(response.data);
-            setLoading(false);
-        }
-        ).catch(error => {
-            console.log(error);
-        });
-    }, []);
-
     return (
         <>
             <Head>
-                <title>Device Types</title>
+                <title>Not found</title>
             </Head>
             <>
                 <Box m="auto" >
@@ -37,24 +18,7 @@ export default function Search() {
         </>
     );
 
-    function search(searchTerm) {
-        if (searchTerm.length > 0) {
-            filterDisplayedData(searchTerm);
-        }
-        else {
-            resetDisplayedData();
-        }
-    }
 
-    function resetDisplayedData() {
-        setSearched("");
-        setDisplayData(data);
-    }
-
-    function filterDisplayedData(searchTerm) {
-        setSearched(searchTerm);
-        setDisplayData(data.filter(row => row.deviceTypeName.toLowerCase().includes(searchTerm.toLowerCase())));
-    }
 
 }
 
